aa370e3a047dbee7b79da45ddfe539fdad897cb2
Kieran <kieranbarry51@gmail.com>
2022-07-12 21:38:30 -0500

    completed parts 1 and 2 of tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Safety Dog" },
+    { id: 2, name: "Danger Cat" },
+    { id: 3, name: "Cookie Munster"},
+    { id: 4, name: "Vacuum Man" },
+    { id: 5, name: "Science Woman" },
+    { id: 6, name: "Junior Java" },
+    { id: 7, name: "Monocle McGiver" },
+    { id: 8, name: "Hero 2: Hero Harder" },
+    { id: 9, name: "Man Bat" },
+    { id: 10, name: "Hero 3: A Good Day to Hero Hard"},
+]
